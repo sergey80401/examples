b71204231d86d7361bd300ec9f59431b53dbfaff
skryuchkov <skryuchkov@gemsdev.com>
2025-05-05 17:40:19 +0600

    some intermediate progress

---
+++ Kafka/UsersLogService/Program.cs
@@ -0,0 +1,46 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+using Options;
+
+var configuration = new ConfigurationBuilder()
+    .AddEnvironmentVariables()
+    .Build();
+
+var kafkaConsumerOptions = configuration.GetSection("KafkaConsumerOptions").Get<KafkaConsumerOptions>() ??
+                           throw new InvalidOperationException("Failed to get settings to connect to Kafka");
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = kafkaConsumerOptions.BootstrapServers,
+    GroupId = kafkaConsumerOptions.GroupId,
+    ClientId = kafkaConsumerOptions.ClientId,
+    AutoOffsetReset = AutoOffsetReset.Latest,
+    EnableAutoCommit = false
+};
+
+using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+consumer.Subscribe(kafkaConsumerOptions.Topic);
+
+try
+{
+    while (true)
+    {
+        try
+        {
+            var consumeResult = consumer.Consume(CancellationToken.None);
+                    
+            Console.WriteLine($"Message received: {consumeResult.Message.Value}");
+            Console.WriteLine($"Partition: {consumeResult.Partition}, Offset: {consumeResult.Offset}");
+
+            consumer.Commit(consumeResult);
+        }
+        catch (ConsumeException e)
+        {
+            Console.WriteLine($"Consumption error: {e.Error.Reason}");
+        }
+    }
+}
+catch (OperationCanceledException)
+{
+    consumer.Close();
+}
--- Kafka/WebApi/Program.cs
+++ Kafka/WebApi/Program.cs
@@ -1,39 +1,86 @@
+using System.Text;
 using System.Text.Json;
 using Confluent.Kafka;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using Models;
+using Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var configuration = new ConfigurationBuilder()
+    .AddEnvironmentVariables()
+    .Build();
+
+
+builder.Services.Configure<KafkaProducerOptions>(options =>
+{
+    var kafkaProducerOptions = configuration.GetSection("KafkaProducerOptions").Get<KafkaProducerOptions>() ??
+                               throw new InvalidOperationException("Failed to get settings to connect to Kafka");
+
+    options.BootstrapServers = kafkaProducerOptions.BootstrapServers;
+    options.ClientId = kafkaProducerOptions.ClientId;
+    options.Topic = kafkaProducerOptions.Topic;
+});
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-}
-
 app.UseHttpsRedirection();
 
-var config = new ProducerConfig
+app.MapPost("/users", async ([FromBody] User user, IOptions<KafkaProducerOptions> kafkaOptions) =>
 {
-    BootstrapServers = "localhost:9092",
-    ClientId = "my-producer-app"
-};
+    var options = kafkaOptions.Value;
 
-app.MapPost("/users", async ([FromBody] User user) =>
-{
+    var config = new ProducerConfig
+    {
+        BootstrapServers = options.BootstrapServers,
+        ClientId = options.ClientId
+    };
+
     using var producer = new ProducerBuilder<string, string>(config)
         .Build();
-    
-    var topic = "test-topic";
+
     var message = new Message<string, string>
     {
-        Key = "message-key",
-        Value = JsonSerializer.Serialize(user)
+        Key = "user:created",
+        Value = JsonSerializer.Serialize(user),
+        Headers = new Headers()
+        {
+            {"message-type", Encoding.UTF8.GetBytes(UsersMessageType.UserCreated.ToString())}
+        }
     };
-    
-    var deliveryResult = await producer.ProduceAsync(topic, message);
-            
+
+    var deliveryResult = await producer.ProduceAsync(options.Topic, message);
+
+    Console.WriteLine($"Delivered message to: {deliveryResult.TopicPartitionOffset}");
+    Console.WriteLine($"Partition: {deliveryResult.Partition}, Offset: {deliveryResult.Offset}");
+});
+
+app.MapGet("/users/{id}", async (int id, IOptions<KafkaProducerOptions> kafkaOptions) =>
+{
+    var options = kafkaOptions.Value;
+
+    var config = new ProducerConfig
+    {
+        BootstrapServers = options.BootstrapServers,
+        ClientId = options.ClientId
+    };
+
+    using var producer = new ProducerBuilder<string, string>(config)
+        .Build();
+
+    var message = new Message<string, string>
+    {
+        Key = "user:read",
+        Value = id.ToString(),
+        Headers = new Headers()
+        {
+            {"message-type", Encoding.UTF8.GetBytes(UsersMessageType.UserRead.ToString())}
+        }
+    };
+
+    var deliveryResult = await producer.ProduceAsync(options.Topic, message);
+
     Console.WriteLine($"Delivered message to: {deliveryResult.TopicPartitionOffset}");
     Console.WriteLine($"Partition: {deliveryResult.Partition}, Offset: {deliveryResult.Offset}");
 });
