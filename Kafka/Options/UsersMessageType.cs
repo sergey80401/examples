4ac433a6282035773ab06528922d3d57cf541795
skryuchkov <skryuchkov@gemsdev.com>
2025-05-06 11:21:27 +0600

    intermediate progress

--- Kafka/Options/UsersMessageType.cs
+++
@@ -1,9 +0,0 @@
-namespace Options;
-
-public enum UsersMessageType
-{
-    UserCreated = 0,
-    UserRead = 1,
-    UsersUpdated = 2,
-    UsersDeleted = 3,
-}
--- Kafka/WebApi/Program.cs
+++ Kafka/WebApi/Program.cs
@@ -27,14 +27,14 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.MapPost("/users", async ([FromBody] User user, IOptions<KafkaProducerOptions> kafkaOptions) =>
+app.MapPost("/users", async ([FromBody] User user, IOptions<KafkaProducerOptions> producerOptions) =>
 {
-    var options = kafkaOptions.Value;
+    var pOptions = producerOptions.Value;
 
     var config = new ProducerConfig
     {
-        BootstrapServers = options.BootstrapServers,
-        ClientId = options.ClientId
+        BootstrapServers = pOptions.BootstrapServers,
+        ClientId = pOptions.ClientId
     };
 
     using var producer = new ProducerBuilder<string, string>(config)
@@ -44,46 +44,12 @@ app.MapPost("/users", async ([FromBody] User user, IOptions<KafkaProducerOptions
     {
         Key = "user:created",
         Value = JsonSerializer.Serialize(user),
-        Headers = new Headers()
-        {
-            {"message-type", Encoding.UTF8.GetBytes(UsersMessageType.UserCreated.ToString())}
-        }
     };
 
-    var deliveryResult = await producer.ProduceAsync(options.Topic, message);
+    var deliveryResult = await producer.ProduceAsync(pOptions.Topic, message);
 
     Console.WriteLine($"Delivered message to: {deliveryResult.TopicPartitionOffset}");
     Console.WriteLine($"Partition: {deliveryResult.Partition}, Offset: {deliveryResult.Offset}");
 });
-
-app.MapGet("/users/{id}", async (int id, IOptions<KafkaProducerOptions> kafkaOptions) =>
-{
-    var options = kafkaOptions.Value;
-
-    var config = new ProducerConfig
-    {
-        BootstrapServers = options.BootstrapServers,
-        ClientId = options.ClientId
-    };
-
-    using var producer = new ProducerBuilder<string, string>(config)
-        .Build();
-
-    var message = new Message<string, string>
-    {
-        Key = "user:read",
-        Value = id.ToString(),
-        Headers = new Headers()
-        {
-            {"message-type", Encoding.UTF8.GetBytes(UsersMessageType.UserRead.ToString())}
-        }
-    };
-
-    var deliveryResult = await producer.ProduceAsync(options.Topic, message);
-
-    Console.WriteLine($"Delivered message to: {deliveryResult.TopicPartitionOffset}");
-    Console.WriteLine($"Partition: {deliveryResult.Partition}, Offset: {deliveryResult.Offset}");
-});
-
 
 app.Run();
